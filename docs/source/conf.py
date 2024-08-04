80ed91495c7ac7d296e7bc1a75bf83232100cb13
smaurice101 <32278116+smaurice101@users.noreply.github.com>
2024-08-04 11:52:22 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'MAADSBML'
+copyright = '2024, Otics Advanced Analytics Incorporated'
+author = 'Sebastian Maurice'
 
 release = '0.1'
 version = '0.1.0'
